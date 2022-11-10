36aca0e2eaf59b6fa2c5bb2d9bb84bb5271dfde3
colantoniogiannfranco <cgiannfranco@rocketmail.com>
2022-11-10 20:58:36 -0300

    entrega-6

--- js/init.js
+++ js/init.js
@@ -7,7 +7,7 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/25801.
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
-
+//Prueba cambios git
 let id_productos = localStorage.getItem('catID');
  
 const LISTADO_AUTOS = `https://japceibal.github.io/emercado-api/cats_products/${id_productos}.json`;
